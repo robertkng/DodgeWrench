1d6717c217105ba9581b4cba4d4751099b06c86d
Robert Ng <robertng85@gmail.com>
2016-10-06 15:31:44 -0400

    added function for horizontal movement of wrench

--- game.js
+++ game.js
@@ -8,6 +8,17 @@ var $wrenchY = $('.wrench').css('top');
 var $wrenchX = $('.wrench').css('left');
 var $speedX = $wrenchX += 5;
 
+function movingWrenchX(){
+  var $wrench = $('.wrench');
+  var $wrenchX = parseInt($('.wrench').css('left'));
+  setInterval(function(){
+    $wrench.css('left', $wrenchX + 'px');
+    if ($wrenchX > 0){
+      $wrenchX += 10;
+    }
+  }, 100);
+};
+
 function movingWrenchY(){
   var $wrench = $('.wrench');
   var $wrenchY = parseInt($('.wrench').css('top'));
@@ -18,6 +29,10 @@ function movingWrenchY(){
     }
   }, 100);
 };
-console.log($wrenchY);
+
+
+
+console.log($wrenchY, $wrenchY);
+movingWrenchX();
 movingWrenchY();
 })
